a19409aa94c5aac5c037efef3c282ef63def325c
sebastianduchene <sebastian.duchene@sydney.edu.au>
2014-08-13 17:02:18 +1000

    completed code. Start runs

---
+++ README.md
@@ -0,0 +1,28 @@
+DRT analyses list
+=================
+
+12 settings:
+
+- **SET1** mean rate: 10^-2, strict clock, calibrations range: 0.5 to 1.5 years
+
+- **SET2** mean rate: 10^-2, strict clock, calibrations range: 5 to 10 years
+
+- **SET3** mean rate: 10^-2, strict clock, calibrations range: 10 to 20 years
+
+- **SET4** mean rate: 10^-2, stdev = 0.3, calibrations range: 0.5 to 1.5 years
+
+- **SET5** mean rate: 10^-2, stdev = 0.3, calibrations range: 5 to 10 years
+
+- **SET6** mean rate: 10^-2, stdev = 0.3, calibrations range: 10 to 20 years
+
+- **SET7** mean rate: 10^-4, strict clock, calibrations range: 0.5 to 1.5 years
+
+- **SET8** mean rate: 10^-4, strict clock, calibrations range: 5 to 10 years
+
+- **SET9** mean rate: 10^-4, strict clock, calibrations range: 10 to 20 years
+
+- **SET10** mean rate: 10^-4, stdev = 0.3, calibrations range: 0.5 to 1.5 years
+
+- **SET11** mean rate: 10^-4, stdev = 0.3, calibrations range: 5 to 10 years
+
+- **SET12** mean rate: 10^-4, stdev = 0.3, calibrations range: 10 to 20 years
---
+++ example_run/run_template.R
@@ -0,0 +1,34 @@
+source('functions.R')
+par(bg = 'black')
+
+mean_rate = 0.001
+sd_rate = 0.01
+min_cal = 1
+max_cal = 1.5
+
+
+
+#for(i in 1:100){
+
+t1 <- get_tree_cal(span_cut = c(1.05, 1.2), max_cal = max_cal, min_cal = min_cal, tr_time = 100, n_tax = 50, print_trees = T)
+p1 <- t1$chronogram
+p1$edge.length <- p1$edge.length * rlnorm(98, log(mean_rate), sd_rate)
+
+s1 <- as.DNAbin(simSeq(p1, l = 1000))
+
+xml1 <- make_xml_file(s1, file_name = 'test1', random_dates = F)
+cat(xml1, file = 'test1.xml', sep = '\n')
+
+run_1 <- run_beast(file_name = 'test1.xml', beast_path = '~/Desktop/progs/Beast2/bin/')
+
+rate_sim <- mean_rate
+root_true <- max(allnode.times(t1$chronogram))
+dates_true <- as.numeric(gsub('^.+_', '', t1$chronogram$tip.label))
+reg_true <- summary(lm(allnode.times(p1, tipsonly = T) ~ dates_true ))
+slope_true <- reg_true$coefficient[2]
+r_true <- reg_true$r.squared
+cal_time <- max(dates_true) - min(dates_true)
+
+cat(paste(cal_time, mean_rate, sd_rate, unlist(run_1), slope_true, r_true, collapse = ' '), sep = '\n', file = 'SET1.txt')
+
+#}
